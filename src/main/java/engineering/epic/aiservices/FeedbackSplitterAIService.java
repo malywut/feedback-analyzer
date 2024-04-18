80a101c8a9a953942c6a16414dce27a4bb63a686
Lize <lize@MacBook-Pro.local>
2024-04-18 23:24:25 +0200

    better prompt template for splitter

--- src/main/java/engineering/epic/aiservices/FeedbackSplitterAIService.java
+++ src/main/java/engineering/epic/aiservices/FeedbackSplitterAIService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @RegisterAiService
 public interface FeedbackSplitterAIService {
     @SystemMessage("""
-            Split the user feedback into coherent parts that treat a similar topic, that can then be addressed separately.
+            Split the user feedback into coherent parts of literal user text that treat a similar topic, that can then be addressed separately.
             If the user treats the same topic in multiple parts of the feedback, make sure to group them together.
             """)
     public List<String> generateAtomicFeedbackComponents(String feedback);
